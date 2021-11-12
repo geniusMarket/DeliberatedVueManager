821b12e5a3316b88f441111deca86021bb0571c4
joi119 <535830592@qq.com>
2021-11-12 14:01:35 +0800

    项目结构初始化

---
+++ vuemanager/src/views/entry.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    2
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "entry",
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuemanager/src/views/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    1
+    <button @click="test()">2222</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "login",
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      test() {
+        this.$router.push("/entry")
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
